9d4a781e0880a66ba4f9ba06ec6403b40f1cd563
jigar <jigarparmarpjp@gmail.com>
2025-07-29 10:47:26 +0530

    Project structure Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1", 
+    author="Jigar Parmar",
+    author_email="jigarparmarpjp@gmail.com",
+    packages=find_packages()
+)
